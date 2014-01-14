4f6de8bb4b18490d7fcae249bd26320977a0822e
jbsneto23 <jbsneto23@gmail.com>
2014-01-14 15:37:14 -0300

    Documentando Puzzle

--- Codigo/puzzle.h
+++ Codigo/puzzle.h
@@ -10,17 +10,63 @@ using namespace std;
 #ifndef PUZZLE_H
 #define PUZZLE_H
 
+/**
+*
+* A classe Puzzle representa o quebra-cabeça. É responsável por ler o arquivo de entrada e validá-lo, gerando o estado inicial.
+*
+* @author João
+* @author Ingrid
+*/
 class Puzzle
 {
     public:
         static const int MAX_SIZE = 80; // tamanho máximo que o tabuleiro pode ter (linhas e colunas)
-        /** Default constructor */
+
+        /**
+        *
+        * Construtor que inicia o estado da classe Puzzle.
+        *
+        * @param file Nome do arquivo do tabuleiro.
+        */
         Puzzle(string file);
-        /** Default destructor */
+
+        /**
+        *
+        * Destrutor que libera a memória alocada pela classe Puzzle.
+        *
+        */
         virtual ~Puzzle();
+
+        /**
+        *
+        * Função que recupera o número de linhas de um tabuleiro.
+        *
+        * @return O número de linhas.
+        */
         int get_rows();
+
+        /**
+        *
+        * Função que recupera o número de colunas de um tabuleiro.
+        *
+        * @return O número de colunas.
+        */
         int get_columns();
+
+        /**
+        *
+        * Função recupera a matriz que representa o tabuleiro.
+        *
+        * @return Matriz de inteiros.
+        */
         int** get_board();
+
+        /**
+        *
+        * Função que recupera a lista de blocos do tabuleiro.
+        *
+        * @return A lista de blocos.
+        */
         vector<Block> get_blocks();
     protected:
     private:
